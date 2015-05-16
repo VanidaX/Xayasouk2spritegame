965e73fbf0bed7a6e7abd66489018d9013b17292
VanidaX <webstudent@PC16286.local>
2015-05-16 11:48:38 -0500

    XayasoukGame
    XayasoukGame

---
+++ Xayasouk2spritegame/GameViewController.swift
@@ -0,0 +1,44 @@
+//
+//  GameViewController.swift
+//  Xayasouk2spritegame
+//
+//  Created by webstudent on 5/16/15.
+//  Copyright (c) 2015 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+extension SKNode {
+    class func unarchiveFromFile(file : NSString) -> SKNode? {
+        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
+            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+}
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
